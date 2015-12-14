ad8f000eab71fa36423200eb27cb8124a94c0e26
usertoblameforyourbugs <tychosvideos@gmail.com>
2015-12-14 09:31:07 +0100

    First version

---
+++ src/binairy/trainer/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binairy.trainer;
+
+/**
+ *
+ * @author Tycho
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Yayframe frame = new Yayframe();
+        frame.setVisible(true);
+    }
+    
+}
